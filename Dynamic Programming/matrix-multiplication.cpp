2317b5c77f040368a5ec68b8d8c718e69ae5b766
Aniket <aniketraj28042003@gmail.com>
2023-10-11 19:12:06 +0530

    added dp files

--- Dynamic Programming/matrix-multiplication.cpp
+++ Dynamic Programming/matrix-multiplication.cpp
@@ -8,16 +8,21 @@ using namespace std;
 template <typename T>
 using vec = vector<T>;
 
-int ftd(vec<int> &v, int i, int j)
+vec<vec<int>> dp;
+
+int ftd(vector<int> &v, int i, int j)
 {
     // base case
     if (i == j || i + 1 == j)
         return 0;
-    int ans = infp;
+    // if(dp[i][j] != -1) return dp[i][j];
+    int ans = INT_MAX;
+
     for (int k = i + 1; k < j; k++)
     {
-        ans = min(ans, ftd(v, i, k) + ftd(v, k, j) + v[i] * v[k] * v[j]);
+        ans = min(ans, ftd(v, i, k) + ftd(v, k, j) + v[i] * v[j] * v[k]);
     }
+    // return dp[i][j] = ans;
     return ans;
 }
 
@@ -30,6 +35,49 @@ int main()
     {
         cin >> v[i];
     }
-    cout << ftd(v, 0, n - 1);
+    dp.resize(1005, vec<int>(1005, 0));
+    // cout << ftd(v, 0, n - 1);
+    //     int matrixChainMultiplication(vec<int> &dims)
+    // {
+    //     int n = dims.size();
+    //     dp.resize(n, vec<int>(n, 0));
+
+    //     for (int len = 2; len < n; len++)
+    //     {
+    //         for (int i = 1; i < n - len + 1; i++)
+    //         {
+    //             int j = i + len - 1;
+    //             dp[i][j] = INT_MAX;
+
+    //             for (int k = i; k < j; k++)
+    //             {
+    //                 dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + dims[i - 1] * dims[k] * dims[j]);
+    //             }
+    //         }
+    //     }
+
+    //     return dp[1][n - 1];
+    // }
+    for (int len = 2; len < n; len++)
+    {
+        for (int i = 1; i + len - 1 < n; i++)
+        {
+            int j = i + len - 1;
+            dp[i][j] = INT_MAX;
+            for (int k = i; k < j; k++)
+            {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + v[i - 1] * v[k] * v[j]);
+            }
+        }
+    }
+    cout << dp[1][n - 1] << "\n";
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            cout << dp[i][j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
