f498971478aa1a1d629fab2c011ed42d337ec104
Aniket <aniketraj28042003@gmail.com>
2024-03-16 19:17:29 +0530

    added codes

---
+++ Linked List/leetcode/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define infp INT_MAX
+#define infn INT_MIN
+#define pp pair<int, int>
+#define mod 1000000007
+
+using namespace std;
+template<typename T>
+using vec = vector<T>;
+
+// https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/description
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        vector<int> arr;
+        while(head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+        ListNode* newHead = NULL;
+        ListNode* dummy = newHead;
+        int n = arr.size();
+        for(int i = 0 ; i < n ; i++) {
+            int sum = 0;
+            bool flag = true;
+            for(int j = i ; j < n ; j++) {
+                sum += arr[j];
+                if(sum == 0) {
+                    flag = false;
+                    i = j;
+                    break;
+                }
+            }
+            if(flag) {
+                ListNode* temp = new ListNode(arr[i]);
+                if(!newHead) {
+                    newHead = temp;
+                    dummy = temp;
+                } else {
+                    dummy->next = temp;
+                    dummy = temp;
+                }
+            }
+        }
+        return newHead;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
