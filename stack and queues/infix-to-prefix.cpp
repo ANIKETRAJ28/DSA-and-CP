776961dad3e435bbf74621b6ddae4608211ca342
Aniket <aniketraj28042003@gmail.com>
2025-02-18 08:45:09 +0530

    add: codes

--- stack and queues/infix-to-prefix.cpp
+++ stack and queues/infix-to-prefix.cpp
@@ -71,7 +71,7 @@ void solve(string &s)
             }
             else
             {
-                while (!st.empty() && priority(s[i]) <= priority(st.top()))
+                while (!st.empty() && priority(s[i]) < priority(st.top()))
                 {
                     output += st.top();
                     st.pop();
