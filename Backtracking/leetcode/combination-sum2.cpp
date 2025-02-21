f65488258f10f0d890afbe6a3840f85a271a0c99
Aniket <aniketraj28042003@gmail.com>
2025-02-21 19:58:06 +0530

    add: codes

--- Backtracking/leetcode/combination-sum2.cpp
+++ Backtracking/leetcode/combination-sum2.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 #define infp INT_MAX
 #define infn INT_MIN
@@ -6,33 +6,40 @@
 #define mod 1000000007
 
 using namespace std;
-template<typename T>
+template <typename T>
 using vec = vector<T>;
 
 // https://leetcode.com/problems/combination-sum-ii/submissions/
 
-class Solution {
+class Solution
+{
 public:
     vector<vector<int>> ans;
-    void f(vector<int>& candidates, vector<int> v, int i, int t) {
-        if(t == 0) {
+    void f(vector<int> &candidates, vector<int> v, int i, int t)
+    {
+        if (t == 0)
+        {
             ans.push_back(v);
             return;
         }
 
-        if(i >= candidates.size()) return;
+        if (i >= candidates.size())
+            return;
 
-        if(candidates[i] <= t) {
+        if (candidates[i] <= t)
+        {
             // pick
             v.push_back(candidates[i]);
-            f(candidates, v, i+1, t-candidates[i]);
+            f(candidates, v, i + 1, t - candidates[i]);
             v.pop_back();
         }
         // not pick
-        while(i+1 < candidates.size() and candidates[i+1] == candidates[i]) i++;
-        f(candidates, v, i+1, t);
+        while (i + 1 < candidates.size() and candidates[i + 1] == candidates[i])
+            i++;
+        f(candidates, v, i + 1, t);
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
         sort(candidates.begin(), candidates.end());
         vector<int> v;
         f(candidates, v, 0, target);
@@ -40,7 +47,43 @@ public:
     }
 };
 
-int main(){
-    
+class Solution
+{
+public:
+    void combinations(vector<vector<int>> &ans, vector<int> &nums, vector<int> &res, int target, int i)
+    {
+        int n = nums.size();
+        if (target == 0)
+        {
+            ans.push_back(res);
+            return;
+        }
+        if (i == n)
+            return;
+        if (nums[i] > target)
+            return;
+        // pick
+        res.push_back(nums[i]);
+        combinations(ans, nums, res, target - nums[i], i + 1);
+        res.pop_back();
+        // not pick
+        int curr = nums[i];
+        while (i < n && nums[i] == curr)
+            i++;
+        combinations(ans, nums, res, target, i);
+    }
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        vector<vector<int>> ans;
+        vector<int> res;
+        sort(candidates.begin(), candidates.end());
+        combinations(ans, candidates, res, target, 0);
+        return ans;
+    }
+};
+
+int main()
+{
+
     return 0;
 }
--- Backtracking/leetcode/combinations.cpp
+++ Backtracking/leetcode/combinations.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 #define infp INT_MAX
 #define infn INT_MIN
@@ -6,35 +6,73 @@
 #define mod 1000000007
 
 using namespace std;
-template<typename T>
+template <typename T>
 using vec = vector<T>;
 
 // https://leetcode.com/problems/combinations/description/
 
-class Solution {
+class Solution
+{
 public:
     vector<vector<int>> ans;
-    void combination(int i, int n, int k, vector<int> v) {
+    void combination(int i, int n, int k, vector<int> v)
+    {
         // base case
-        if(v.size() == k) {
+        if (v.size() == k)
+        {
             ans.push_back(v);
             return;
         }
 
-        for(int j = i ; j <= n ; j++) {
+        for (int j = i; j <= n; j++)
+        {
             v.push_back(j);
-            combination(j+1, n, k, v);
+            combination(j + 1, n, k, v);
             v.pop_back();
         }
-    } 
-    vector<vector<int>> combine(int n, int k) {
+    }
+    vector<vector<int>> combine(int n, int k)
+    {
         vector<int> v;
-        combination(1, n, k,v);
+        combination(1, n, k, v);
         return ans;
     }
 };
 
-int main(){
-    
+class Solution
+{
+public:
+    void combination(vector<vector<int>> &ans, vector<int> &nums, vector<int> &res, int k, int i)
+    {
+        int n = nums.size();
+        if (res.size() == k)
+        {
+            ans.push_back(res);
+            return;
+        }
+        if (i == n)
+            return;
+        // pick
+        res.push_back(nums[i]);
+        combination(ans, nums, res, k, i + 1);
+        res.pop_back();
+        // not pick
+        combination(ans, nums, res, k, i + 1);
+    }
+    vector<vector<int>> combine(int n, int k)
+    {
+        vector<vector<int>> ans;
+        vector<int> res;
+        vector<int> nums;
+        for (int i = 1; i <= n; i++)
+            nums.push_back(i);
+        combination(ans, nums, res, k, 0);
+        return ans;
+    }
+};
+
+int main()
+{
+
     return 0;
 }
