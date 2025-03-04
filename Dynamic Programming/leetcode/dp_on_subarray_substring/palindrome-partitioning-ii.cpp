489756cbb523db202540d79561961087b1207e44
Aniket <aniketraj28042003@gmail.com>
2025-03-04 19:35:50 +0530

    add: codes

--- Dynamic Programming/leetcode/dp_on_subarray_substring/palindrome-partitioning-ii.cpp
+++ Dynamic Programming/leetcode/dp_on_subarray_substring/palindrome-partitioning-ii.cpp
@@ -1,3 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define infp INT_MAX
+#define infn INT_MIN
+#define pp pair<int, int>
+#define mod 1000000007
+
+using namespace std;
+template <typename T>
+using vec = vector<T>;
+
 class Solution
 {
 public:
@@ -65,3 +76,9 @@ public:
     return fbu(s);
   }
 };
+
+int main()
+{
+
+  return 0;
+}
