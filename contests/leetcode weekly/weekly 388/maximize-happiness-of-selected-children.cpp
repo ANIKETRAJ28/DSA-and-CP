5c87a88ae2b9c3dcb1ddc7d1620e82f09056d276
Aniket <aniketraj28042003@gmail.com>
2025-04-12 19:09:05 +0530

    add: codes

--- contests/leetcode weekly/weekly 388/maximize-happiness-of-selected-children.cpp
+++ contests/leetcode weekly/weekly 388/maximize-happiness-of-selected-children.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 #define infp INT_MAX
 #define infn INT_MIN
@@ -6,33 +6,59 @@
 #define mod 1000000007
 
 using namespace std;
-template<typename T>
+template <typename T>
 using vec = vector<T>;
 
 // https://leetcode.com/problems/maximize-happiness-of-selected-children/
 
-class Solution {
+class Solution
+{
 public:
-    long long maximumHappinessSum(vector<int>& happiness, int k) {
+    long long maximumHappinessSum(vector<int> &happiness, int k)
+    {
         int cnt = 0;
         long long ans = 0;
         sort(happiness.begin(), happiness.end());
         int n = happiness.size();
-        for(int i = n-1 ; i >= 0 ; i--) {
+        for (int i = n - 1; i >= 0; i--)
+        {
             happiness[i] -= cnt;
-            if(happiness[i] < 0) happiness[i] = 0;
+            if (happiness[i] < 0)
+                happiness[i] = 0;
             cnt++;
         }
-        for(int i = n-1 ; i >= 0 ; i--) {
+        for (int i = n - 1; i >= 0; i--)
+        {
             ans += happiness[i];
             k--;
-            if(k == 0) return ans;
+            if (k == 0)
+                return ans;
         }
         return ans;
     }
 };
 
-int main() {
-    
+class Solution
+{
+public:
+    long long maximumHappinessSum(vector<int> &happiness, int k)
+    {
+        int n = happiness.size();
+        sort(happiness.begin(), happiness.end());
+        long long ans = 0;
+        int cnt = 0;
+        for (int i = n - 1; i >= 0 && k > 0; i--)
+        {
+            ans += max(0, happiness[i] - cnt);
+            cnt++;
+            k--;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
     return 0;
 }
