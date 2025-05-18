cd352a8257a3d646a4d767f01a6e5334638d39e8
Aniket <aniketraj28042003@gmail.com>
2025-05-18 11:54:30 +0530

    add: codes

---
+++ greedy/leetcode/break-a-palindrome.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define infp INT_MAX
+#define infn INT_MIN
+#define pp pair<int, int>
+#define mod 1000000007
+
+using namespace std;
+template <typename T>
+using vec = vector<T>;
+
+//  https://leetcode.com/problems/break-a-palindrome/
+
+class Solution
+{
+public:
+  string breakPalindrome(string s)
+  {
+    int n = s.size();
+    if (n == 1)
+      return "";
+    int i = 0, j = n - 1;
+    while (i <= j)
+    {
+      if (s[i] != s[j])
+        return s;
+      if (i == n / 2)
+        break;
+      if (s[i] == s[j])
+      {
+        if (s[i] == 'a')
+        {
+          i++;
+          j--;
+          continue;
+        }
+        s[i] = 'a';
+        return s;
+        i++;
+        j--;
+      }
+    }
+    s[n - 1] = s[n - 1] + 1;
+    return s;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
