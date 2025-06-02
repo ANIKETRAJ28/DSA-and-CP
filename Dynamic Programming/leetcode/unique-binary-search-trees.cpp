da6ade6603a72b40c4a06859566c893a77c76e39
Aniket <aniketraj28042003@gmail.com>
2025-06-02 20:47:43 +0530

    add: codes

--- Dynamic Programming/leetcode/unique-binary-search-trees.cpp
+++ Dynamic Programming/leetcode/unique-binary-search-trees.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 #define infp INT_MAX
 #define infn INT_MIN
@@ -6,40 +6,68 @@
 #define mod 1000000007
 
 using namespace std;
-template<typename T>
+template <typename T>
 using vec = vector<T>;
 
 // https://leetcode.com/problems/unique-binary-search-trees/description/
 
-class Solution {
+class Solution
+{
 public:
     vector<int> dp;
-    int ftd(int n) {
-        if(n == 0 or n == 1) return 1;
-        if(n == 2) return 2;
-        if(dp[n] != -1) return dp[n];
-        int res = 0;
-        for(int i = 1 ; i <= n ; i++) {
-            res += ftd(i-1)*ftd(n-i);
+    int f(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            ans += f(i - 1) * f(n - i);
         }
-        return dp[n] = res;
-    } 
-    int numTrees(int n) {
-        // dp.resize(n+1, -1);
-        // return ftd(n);
-        dp.resize(n+2, 0);
-        dp[0] = dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3 ; i <= n ; i++) {
-            for(int k = 1 ; k <= i ; k++) {
-                dp[i] += dp[k-1]*dp[i-k];
+        return ans;
+    }
+    int ftd(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            ans += ftd(i - 1) * ftd(n - i);
+        }
+        return dp[n] = ans;
+    }
+    int fbu(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        dp.resize(20, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            int ans = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                ans += dp[j - 1] * dp[i - j];
             }
+            dp[i] = ans;
         }
         return dp[n];
     }
+    int numTrees(int n)
+    {
+        // return f(n);
+        // dp.resize(20, -1);
+        // return ftd(n);
+        return fbu(n);
+    }
 };
 
-int main() {
-    
+int main()
+{
+
     return 0;
 }
